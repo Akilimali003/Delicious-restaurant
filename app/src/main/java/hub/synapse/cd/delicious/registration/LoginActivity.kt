41313cedc65e288a60f25f5c5e53c08276f33cc2
Akilimali Samuel Vijey <Akilimalisamuelvijey@gmail.com>
2019-12-08 21:36:36 +0200

    Correction des bugs

--- app/src/main/java/hub/synapse/cd/delicious/registration/LoginActivity.kt
+++ app/src/main/java/hub/synapse/cd/delicious/registration/LoginActivity.kt
@@ -5,60 +5,100 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import hub.synapse.cd.delicious.R
 import hub.synapse.cd.delicious.views.MainActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var editTextEmailLogin : TextInputEditText
+    lateinit var editTextPasswordLogin : TextInputEditText
+    lateinit var loginButton : Button
+    lateinit var progDialog : ProgressDialog
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val progDialog = ProgressDialog(this@LoginActivity)
+        editTextEmailLogin = findViewById(R.id.email_login)
+        editTextPasswordLogin = findViewById(R.id.password_login)
+        loginButton = findViewById(R.id.login_button)
+
+        progDialog = ProgressDialog(this@LoginActivity)
 
         tv_nothaveyetanaccount_login.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
 
-        login_button.setOnClickListener {
-
-            progDialog.setTitle("Connexion")
-            progDialog.setMessage("Patientez svp...")
-            progDialog.show()
+        loginButton.setOnClickListener {
             //call the method
-            performeLogin()
+            loginFunction()
         }
 
     }
 
     private fun performeLogin(){
-        val email = email_login.text.toString()
-        val password = password_login.text.toString()
+        val email = editTextEmailLogin.text.toString()
+        val password = editTextPasswordLogin.text.toString()
 
-        //if mail or password is empty
-        if(email.isEmpty() || password.isEmpty()){
-            Toast.makeText(this, "Completer l'email et le mot de passe s'il vous plait !", Toast.LENGTH_LONG).show()
-            return
-        }
-        //Firebase Authentication to create a user with email and password
+        //Firebase Authentication sign in with email and password
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
             .addOnCompleteListener {
                 if(!it.isSuccessful){
+                    progDialog.dismiss()
+                    editTextPasswordLogin.setText("")
                     return@addOnCompleteListener
                 } else {
+                    editTextEmailLogin.setText("")
+                    editTextPasswordLogin.setText("")
+                    progDialog.dismiss()
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                 }
                 //kill the login activity in the memory
                 finish()
             }.addOnFailureListener{
-                Toast.makeText(this,  "Impossible de vous logger: ${it.message}", Toast.LENGTH_LONG).show()
+                Toast.makeText(this,  "Impossible de vous connecter: ${it.message}", Toast.LENGTH_LONG).show()
             }
+    }
+
+    private fun loginFunction(){
+        val strEmail = editTextEmailLogin.text.toString().trim()
+        val strPassword = editTextPasswordLogin.text.toString().trim()
+
+        if (TextUtils.isEmpty(strEmail) || TextUtils.isEmpty(strPassword)){
+            editTextEmailLogin.error = "Champs vide"
+            editTextPasswordLogin.error = "Champs vide"
+
+        } else if (!emailValidationFunction(strEmail)){
+            editTextEmailLogin.error = "Adresse mail invalide"
+
+        } else {
+            //call the method
+            performeLogin()
+
+            progDialog.setTitle("Connexion")
+            progDialog.setMessage("Patientez svp...")
+            progDialog.show()
+
+        }
+    }
+
+    private fun emailValidationFunction(strEmail : String) : Boolean{
+        val EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val pattern : Pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher : Matcher = pattern.matcher(strEmail)
+        return matcher.matches()
     }
 }
--- app/src/main/java/hub/synapse/cd/delicious/registration/RegisterActivity.kt
+++ app/src/main/java/hub/synapse/cd/delicious/registration/RegisterActivity.kt
@@ -1,52 +1,97 @@
 package hub.synapse.cd.delicious.registration
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.text.TextUtils
+import android.widget.Button
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import hub.synapse.cd.delicious.R
+import hub.synapse.cd.delicious.views.MainActivity
 import kotlinx.android.synthetic.main.activity_register.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var editTextEmailRegister: TextInputEditText
+    lateinit var editTextPasswordRegister: TextInputEditText
+    lateinit var registerButton: Button
+    lateinit var progDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        editTextEmailRegister = findViewById(R.id.register_email)
+        editTextPasswordRegister = findViewById(R.id.register_password)
+        registerButton = findViewById(R.id.register_button)
+
+        progDialog = ProgressDialog(this@RegisterActivity)
 
         tv_already_register_register.setOnClickListener {
             //kill the activity
             finish()
         }
 
-        register_button.setOnClickListener {
+        registerButton.setOnClickListener {
             //call the method
-            performeRegister()
+            registerFunction()
         }
     }
 
-    //register function
-    private fun performeRegister(){
-        val email = register_email.text.toString()
-        val password = register_password.text.toString()
+    private fun performeRegister() {
+        val email = editTextEmailRegister.text.toString()
+        val password = editTextPasswordRegister.text.toString()
 
-        //if mail or password is empty
-        if(email.isEmpty() || password.isEmpty()){
-            Toast.makeText(this, "Completer l'email et le mot de passe s'il vous plait !", Toast.LENGTH_LONG).show()
-            return
-        }
         //Firebase Authentication to create a user with email and password
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener {
-                if(!it.isSuccessful){
+                if (!it.isSuccessful) {
+                    progDialog.dismiss()
+                    editTextPasswordRegister.setText("")
                     return@addOnCompleteListener
                 } else {
-//                    Toast.makeText(this, "Bienvenu", Toast.LENGTH_LONG).show()
+                    editTextEmailRegister.setText("")
+                    editTextPasswordRegister.setText("")
+                    progDialog.dismiss()
                     val intent = Intent(this, LoginActivity::class.java)
                     startActivity(intent)
                 }
-            }.addOnFailureListener{
-                Toast.makeText(this,  "Impossible de créer un compte: ${it.message}", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Impossible de créer un compte: ${it.message}", Toast.LENGTH_LONG).show()
             }
+    }
+
+    private fun registerFunction(){
+        val strEmail = editTextEmailRegister.text.toString().trim()
+        val strPassword = editTextPasswordRegister.text.toString().trim()
+
+        if (TextUtils.isEmpty(strEmail) || TextUtils.isEmpty(strPassword)){
+            editTextEmailRegister.error = "Champs vide"
+            editTextPasswordRegister.error = "Champs vide"
+
+        } else if (!emailValidationFunction(strEmail)){
+            editTextEmailRegister.error = "Adresse mail invalide"
+
+        } else {
+            //call the method
+            performeRegister()
+
+            progDialog.setTitle("Création du compte")
+            progDialog.setMessage("Patientez svp...")
+            progDialog.show()
+
+        }
+    }
+
+    private fun emailValidationFunction(strEmail : String) : Boolean{
+        val EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val pattern : Pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher : Matcher = pattern.matcher(strEmail)
+        return matcher.matches()
     }
 }
